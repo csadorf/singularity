3ec9e32cde77051b2f96a88555591990a82aed07
Carl Simon Adorf <csadorf@umich.edu>
2018-05-18 09:38:32 -0400

    Rename the template to 'default'.
    To avoid automatic build of the template on singularity hub.

--- make_recipes.py
+++ make_recipes.py
@@ -5,7 +5,7 @@ import jinja2
 
 if __name__ == '__main__':
     env = jinja2.Environment(loader=jinja2.FileSystemLoader('templates'))
-    template = env.get_template('Singularity')
+    template = env.get_template('default')
     for cuda in (False, True):
         for system in ('flux', 'comet', 'bridges'):
             tag = 'cuda8-' + system if cuda else system
